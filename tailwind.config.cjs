f5d473412eae995767f7e1d03b079e68a526b282
Shanxx <96603980+notshanxx@users.noreply.github.com>
2023-04-10 17:40:46 +0800

    v2

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      workSans: ["Work Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
