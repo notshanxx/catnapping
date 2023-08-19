ec2951be76996e7da3e8be33c8e4a315dd0ec35d
marco <manuelmarkvince@gmail.com>
2023-08-19 14:15:47 +0800

    change in package name

--- src/Popup.svelte
+++
@@ -1,5 +0,0 @@
-
-<div class="absolute top-0 right-0 flex items-center justify-center z-20" >
-    <p class="bg-orange-600 text-white rounded-sm p-2 text-center text-sm opacity-90"><slot></slot></p>
-    
-</div>  
